23f7656844c434f5d1a07cc1068cf848135f00fc
chosterm <arianfiroozi3@gmail.com>
2024-03-28 16:08:25 +0330

    organized local p2p in a class, text message passed by method

--- main.cpp
+++ main.cpp
@@ -3,50 +3,116 @@
 #include <QApplication>
 #include <QtMultimedia>
 #include <QtMultimedia/QMediaPlayer>
+
+#include<chrono>
+#include<thread>
+
 #include "rtc/rtc.hpp"
 #include "rtc/description.hpp"
 #include "rtc/datachannel.hpp"
 
 using namespace std;
 
-void logPc(rtc::PeerConnection *pc, int id)
-{
-    pc->onIceStateChange([id](rtc::PeerConnection::IceState iceState){
-        cout << id <<"icestate changed:"<<iceState<<endl;
-    });
+class Peer{
+private:
+    int id;
+    rtc::Configuration config;
+    shared_ptr<rtc::DataChannel> dc;
+    shared_ptr<rtc::DataChannel> incomingDc;
+    int state;
 
-    pc->onStateChange([id](rtc::PeerConnection::State state) {
-        std::cout << id << "State: " << state << std::endl;
-    });
+    void logPc(rtc::PeerConnection *pc, int id)
+    {
+        pc->onIceStateChange([id](rtc::PeerConnection::IceState iceState){
+            cout << id <<" icestate changed: "<<iceState<<endl;
+        });
 
-    pc->onGatheringStateChange([id](rtc::PeerConnection::GatheringState state) {
-        std::cout << id << "Gathering state: " << state << std::endl;
-    });
+        pc->onSignalingStateChange([id](rtc::PeerConnection::SignalingState state){
+            cout << id <<" signaling changed: "<<state<<endl;
+        });
 
-    pc->onLocalDescription([pc](rtc::Description sdp) {
-        // Send the SDP to the remote peer
-        // cout << id<<sdp.generateSdp()<<endl;
-        pc->setRemoteDescription(rtc::Description(sdp));
-    });
+        pc->onStateChange([id](rtc::PeerConnection::State state) {
+            std::cout << id << " state changed: " << state << std::endl;
+        });
 
-    pc->onLocalCandidate([pc](rtc::Candidate candidate) {
-        // Send the candidate to the remote peer
-        pc->addRemoteCandidate(rtc::Candidate(candidate.candidate(), candidate.mid()));
-    });
-}
+        pc->onGatheringStateChange([id](rtc::PeerConnection::GatheringState state) {
+            std::cout << id << " gathering state changed: " << state << std::endl;
+        });
 
-void logDc(shared_ptr<rtc::DataChannel> dc, int id)
-{
-    dc->onOpen([id]() {
-        std::cout << id << "datachannel Open" << std::endl;
-    });
+        pc->onLocalDescription([pc](rtc::Description sdp) {
+            // cout << id<<sdp.generateSdp()<<endl;
+            pc->setRemoteDescription(rtc::Description(sdp));
+        });
 
-    dc->onMessage([id](std::variant<rtc::binary, rtc::string> message) {
-        if (std::holds_alternative<rtc::string>(message)) {
-            std::cout << id << "message received: " << get<rtc::string>(message) << std::endl;
-        }
-    });
-}
+        pc->onLocalCandidate([pc](rtc::Candidate candidate) {
+            pc->addRemoteCandidate(rtc::Candidate(candidate.candidate(), candidate.mid()));
+        });
+    }
+
+    void logDc(shared_ptr<rtc::DataChannel> dc, int id)
+    {
+        dc->onOpen([id, this]() {
+            std::cout << id << " datachannel open" << std::endl;
+            state = 1; //shouldn't be here
+        });
+
+        dc->onMessage([id](std::variant<rtc::binary, rtc::string> message) {
+            if (std::holds_alternative<rtc::string>(message)) {
+                std::cout << id << " message received: " << get<rtc::string>(message) << std::endl;
+            }
+        });
+    }
+
+public:
+    rtc::PeerConnection* pc; //should be private in final
+
+    Peer(int _id)
+    {
+        id = _id;
+        state = 0; //TODO enum
+        config.iceServers.emplace_back("stun:stun.l.google.com:19302");
+        pc = new rtc::PeerConnection(config);
+    }
+
+    void printLog()
+    {
+        logPc(pc, id);
+        logDc(dc, id);
+    }
+
+    void createDataChannel(std::string name)
+    {
+        dc = pc->createDataChannel(name);
+
+        pc->onDataChannel([this](shared_ptr<rtc::DataChannel> incoming) {
+            incomingDc = incoming;
+            incomingDc->send(to_string(id) + " is connected");
+        });
+    }
+
+    void sendMsg(std::string msg)
+    {
+        incomingDc->send(msg);
+    }
+
+    void setLocalToOffer()
+    {
+        pc->setLocalDescription(rtc::Description::Type::Offer);
+    }
+
+    rtc::Description getSdp()
+    {
+        return pc->localDescription().value().generateSdp();
+    }
+
+    void setRemoteDescription(rtc::Description sdp) {
+        pc->setRemoteDescription(sdp);
+    }
+
+    int isConnected(){
+        return state;
+    }
+};
 
 int main(int argc, char *argv[])
 {
@@ -55,46 +121,29 @@ int main(int argc, char *argv[])
 
     // rtcInitLogger(rtcLogLevel::RTC_LOG_DEBUG, NULL);
 
-    rtc::Configuration config;
-    config.iceServers.emplace_back("stun:stun.l.google.com:19302");
+    Peer firstPeer(1);
+    Peer secondPeer(2);
 
-    rtc::PeerConnection pc(config);
-    logPc(&pc, 1);
+    firstPeer.createDataChannel("recieve");
+    firstPeer.setLocalToOffer();
 
-    cout<<"signaling state:"<<pc.signalingState()<<endl;
+    secondPeer.setRemoteDescription(firstPeer.getSdp());
+    secondPeer.createDataChannel("send");
+    secondPeer.setLocalToOffer();
 
-    shared_ptr<rtc::DataChannel> dc = pc.createDataChannel("test");
-    logDc(dc, 1);
+    firstPeer.setRemoteDescription(secondPeer.getSdp());
 
-    /// pc1 ///////////////////////////////////
+    firstPeer.printLog();
+    secondPeer.printLog();
 
-    rtc::PeerConnection pc2(config);
-    logPc(&pc2, 2);
+    while(!firstPeer.isConnected() || !secondPeer.isConnected()) //wait till connect
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
 
-    cout<<"signaling state:"<<pc2.signalingState()<<endl;
+    firstPeer.sendMsg("first peer communicating");
+    secondPeer.sendMsg("second peer communicating");
 
-
-    pc.setLocalDescription(rtc::Description::Type::Offer);
-
-    rtc::Description d = rtc::Description(pc.localDescription().value().generateSdp());
-    // cout<<pc.localDescription().value().generateSdp()<<endl;
-    // cout<<pc2.localDescription().value().generateSdp()<<endl;
-    pc2.setRemoteDescription(d);
-
-    shared_ptr<rtc::DataChannel> dc2 = pc2.createDataChannel("test2");
-    logDc(dc2, 2);
-
-    pc2.onDataChannel([](shared_ptr<rtc::DataChannel> incoming) {
-        incoming->send("Hello from2!");
-    });
-
-    pc2.setLocalDescription(rtc::Description::Type::Offer); //?
-
-    pc.setRemoteDescription(pc2.localDescription().value().generateSdp());
-
-    pc.onDataChannel([](shared_ptr<rtc::DataChannel> incoming) {
-        incoming->send("Hello from1!");
-    });
 
     w.show();
     return a.exec();
